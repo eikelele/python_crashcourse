64b8e20fd078680dd639446b79fcf4892583d1cd
eikelele <dereisa25@gmail.com>
2019-11-12 09:26:49 +0100

    Added missing release

---
+++ p25_p29_try_it_yourself.py
@@ -0,0 +1,34 @@
+#Name: Eike Saathoff Date: 08.11.2019impor
+#2-3 Personal Message
+first_name = 'eike'
+last_name = 'saathoff'
+full_name = f"{first_name} {last_name}"
+message = f"Hey {full_name.title()}! How are you today?"
+print(message)
+#2-4 Name Cases
+lower_full_name = full_name.lower()
+upper_full_name = full_name.upper()
+title_full_name = full_name.title()
+print(title_full_name)
+print(lower_full_name)
+print(upper_full_name)
+#2-5 Famous Quote + 2-6 Famous Quote 2
+famous_person = 'Albert Einstein'
+quote = f"{famous_person} once said, \"A person who never made a mistake never tried anything new.\""
+print(quote)
+#2-7 Stripping names
+full_name = ' eike saathoff '
+print(full_name)
+strip_full_name = full_name.strip()
+print(strip_full_name)
+
+#page 29 2-8 Number Eight
+print(5+3)
+print(10-2)
+print(2*4)
+print(2**3)
+print(80/10)
+
+fav_number= 36
+message = f"My favourite number is {fav_number}!"
+print(message)
---
+++ testing.py
@@ -0,0 +1,22 @@
+message = 'Hello Python world!'
+print(message)
+
+mesage = 'Hello Python Crash Course world!'
+print(mesage)
+
+test_message = 'Test123'
+print(test_message)
+
+messages = 'My name is Eike.'
+print(messages)
+messages = 'My name is Silke.'
+print(messages)
+
+name = 'ada lovelace'
+print(name.title())
+
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
